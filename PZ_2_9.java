42f8f83008093f79c2669736c47f15b3986cd1bd
Emi-Chan394 <olga.gnt.vl@gmail.com>
2025-09-08 12:17:51 +0300

    Add files via upload

---
+++ PZ_2_9.java
@@ -0,0 +1,17 @@
+package PZ_2;
+
+import java.util.Scanner;
+
+public class PZ_2_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = 109;
+        System.out.print("Введите скорость автомобиля (V -> км/час): ");
+        float v = sc.nextFloat();
+        System.out.print("Введите время поездки (t -> часов): ");
+        float t = sc.nextFloat();
+        
+        System.out.println("За " + t + " часов, со скоростью " + v + " км/час, на кольцевой дороге длинной " + s + " километров, автомобиль был бы на отметке: " + (((s + (v * t % s)) % s) - ((s + (v * t % s)) % s) % 1));
+        sc.close();
+    }
+}
